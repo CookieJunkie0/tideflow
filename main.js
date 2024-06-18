25c07e9512e08f8c16a74336e038a25657e858ba
CookieJunkie0 <120581656+CookieJunkie0@users.noreply.github.com>
2024-06-18 22:06:22 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,75 +1,82 @@
 const { io } = require("socket.io-client");
 const fs = require('fs');
+const { SocksProxyAgent } = require('socks-proxy-agent');
 const axios = require('axios-https-proxy-fix');
 const { setupAccount, sleep, randomInt, getAccountData, getCaptchaToken } = require('./utils.js');
 const { ethers } = require("ethers");
+const { resolve } = require("path");
 require('dotenv').config({ path: './.env' });
 
 const STARTUP_WAIT = JSON.parse(process.env.STARTUP_WAIT);
 const BUY_WAIT = JSON.parse(process.env.BUY_WAIT);
 const GAME_WAIT = JSON.parse(process.env.GAME_WAIT);
+const TRADES = JSON.parse(process.env.TRADES);
+const TRADES_DELAY = JSON.parse(process.env.TRADES_DELAY);
 
 async function startCycle(accounts) {
   try {
-      for(const account of accounts) {
-        const socket = account.socket;
-        let gameId = null;
+      accounts.map(async account => { 
 
-        socket.on('connect', async () => {
-          console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Connected to socket - ${socket.id}`);
-          // await sleep(randomInt(STARTUP_WAIT[0], STARTUP_WAIT[1]) * 1000);
+        await sleep(randomInt(STARTUP_WAIT[0], STARTUP_WAIT[1]) * 1000);
+        
+        while(true) {
           const captchaToken = await getCaptchaToken(account.proxy);
-          socket.emit("play-vs-opponent", {
-            token: captchaToken.token
+          const socket = io("wss://api.thegame.tideflow.com/game", { autoConnect: true, query: {captchaToken: captchaToken.token, gameMode: "play-vs-opponent"}, path: `/socket.io`, auth: { token: account.token }, agent: new SocksProxyAgent(account.proxy) });
+          let gameId = null;
+
+          socket.on('initial-data', (data) => {
+            gameId = data.sessionId;
+
+            console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Started new game - ID: ${gameId}`);
           })
-        });
+          
+          const endgame = new Promise(resolve => {
+            socket.on('disconnect', async (reason) => {
+              await sleep(randomInt(GAME_WAIT[0], GAME_WAIT[1]) * 1000);
+              resolve("end")
+            });
+          });
 
-        socket.on('initial-data', (data) => {
-          gameId = data.sessionId;
+          socket.on('error', (err) => {
+            console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Socket error - ${err}`);
+          });
 
-          console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Started new game - ID: ${gameId}`);
-        })
+          socket.on('remaining-time', async (time) => {
+            if(time === 0) {
 
-        socket.on('disconnect', (reason) => {
-          console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Disconnected from socket, reason: ${reason}`);
-          socket.connect();
-        });
+              await sleep(randomInt(BUY_WAIT[0], BUY_WAIT[1]));
+              const trades = randomInt(TRADES[0], TRADES[1])
 
-        socket.on('error', (err) => {
-          console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Socket error - ${err}`);
-        });
+              for(let i = 0; i < trades; i++) {
+                console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Trading ${i+1}/${trades}`);
+                socket.emit("buy", gameId)
+                await sleep(randomInt(TRADES_DELAY[0], TRADES_DELAY[1]) * 1000);
+                socket.emit("sell", gameId)
+              }
 
-        socket.on('remaining-time', async (time) => {
+              console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Made ${trades} trades`);
+            }
+          });
 
-          if(time === 0) {
-            await sleep(randomInt(BUY_WAIT[0], BUY_WAIT[1]));
-            socket.emit("buy", gameId)
-          }
-        });
+          socket.on('match-ended', async (data) => {
+            console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Game ended! Result: ${data.result}, shells collected: ${data.points}`);
+            const accountData = await getAccountData(account.instance, account.token);
+            if(accountData.success) {
+              console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Total shells: ${accountData.data.score}`);
+            }
+          });
 
-        socket.on('match-ended', async (data) => {
-          console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Game ended! Result: ${data.result}, shells collected: ${data.points}`);
-          const accountData = await getAccountData(account.instance, account.token);
-          if(accountData.success) {
-            console.log(`${new Date().toLocaleTimeString()} ${account.wallet.address} | Total shells: ${accountData.data.score}`);
-          }
-          // await sleep(randomInt(GAME_WAIT[0], GAME_WAIT[1]) * 1000);
+          socket.connect()
 
-          const captchaToken = await getCaptchaToken(account.proxy);
-          socket.emit("play-vs-opponent", {
-            token: captchaToken.token
-          })
-        });
-
-
-        socket.connect()
-      }
-  } catch(e) {return {success: false, err: e}}
+          await endgame;
+        }
+      })
+  } catch(e) {console.log(e); return {success: false, err: e}}
 }
 
 async function main() {
-  const wallets = fs.readFileSync('wallets.txt').toString().split(/\r?\n/);
-  const proxies = process.env.PROXY || fs.readFileSync('proxy.txt').toString().split(/\r?\n/);
+  const wallets = fs.readFileSync('wallets.txt').toString().split('\n');
+  const proxies = process.env.PROXY || fs.readFileSync('proxy.txt').toString().split('\n');
 
   const accounts = [];
   for (let i = 0; i < wallets.length; i++) {
